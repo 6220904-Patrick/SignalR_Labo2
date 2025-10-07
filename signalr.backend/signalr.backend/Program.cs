c41b4da1096fa198dd89ec1cc1ca4b0bd692c494
Patrick Borca <pborca@gmail.com>
2025-10-06 20:23:23 -0400

    BUGFIX Server address

--- signalr.backend/signalr.backend/Program.cs
+++ signalr.backend/signalr.backend/Program.cs
@@ -31,7 +31,8 @@ builder.Services.AddCors(options =>
         .AllowCredentials());
 });
 
-string serverAdress = "http://localhost:5106";
+// 5106 is wrong address
+string serverAdress = "https://localhost:7060";
 
 SymmetricSecurityKey signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("C'est tellement la meilleure cle qui a jamais ete cree dans l'histoire de l'humanite (doit etre longue)"));
 
